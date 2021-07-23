80ba73c1b7a9ef675a61f5f718fcaf8ea0bbea7d
Luiz Fernando Martins Leite <luizfernandoleite@gmail.com>
2021-07-23 20:47:00 -0300

    inserting Schema

--- src/http.ts
+++ src/http.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { createServer } from "http";
+import mongoose from "mongoose";
 import path from "path";
 import { Server } from "socket.io";
 
 const app = express();
 
 const server = createServer(app); // para utilizar o socket e o express
+
+mongoose.connect("mongodb://localhost/chatSocket", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
---
+++ src/schemas/ChatRoom.ts
@@ -0,0 +1,24 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { v4 as uuid } from "uuid";
+
+import { User } from "./User";
+
+type ChatRoom = Document & {
+  idUsers: User[];
+  idChatRoom: string;
+};
+
+const ChatRoomSchema = new Schema({
+  isUsers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Users",
+    },
+  ],
+  idChatRoom: {
+    type: String,
+    default: uuid(),
+  },
+});
+
+const ChatRoom = mongoose.model<ChatRoom>("ChatRoom", ChatRoomSchema);
